af00759eef871e82143359031de30b95d2c57d32
Ayoub Aguert <ayoub.aguert@gmail.com>
2020-01-14 15:11:18 +0100

    adding try catch

--- computor.py
+++ computor.py
@@ -7,25 +7,28 @@ from computer_v.main.read_equation import get_user_input, read_equation
 from computer_v.math.polynomial import Polynomial, D2plynominal, D1plynominal
 import computer_v.glob as g
 
-equation = read_equation()
+try:
+    equation = read_equation()
 #parisng var name
-g.varname = re.search(r"[a-zA-Z]+", equation).group(0)
-equation = re.sub(r"[a-zA-Z]+", "x", equation)
-left, right = equation.split("=")
+    g.varname = re.search(r"[a-zA-Z]+", equation).group(0)
+    equation = re.sub(r"[a-zA-Z]+", "x", equation)
+    left, right = equation.split("=")
 
-eq_poly = Polynomial.fromexpr(left) - Polynomial.fromexpr(right)
+    eq_poly = Polynomial.fromexpr(left) - Polynomial.fromexpr(right)
 
-if (eq_poly.deg == 2):
-    D2plynominal(eq_poly.coefs).solve()
-elif (eq_poly.deg == 1):
-    D1plynominal(eq_poly.coefs).solve()
-elif (eq_poly.deg == 0):
-    if (eq_poly.coefs[0] == 0):
-        print("all numbers are solutions to this eqation")
+    if (eq_poly.deg == 2):
+        D2plynominal(eq_poly.coefs).solve()
+    elif (eq_poly.deg == 1):
+        D1plynominal(eq_poly.coefs).solve()
+    elif (eq_poly.deg == 0):
+        if (eq_poly.coefs[0] == 0):
+            print("all numbers are solutions to this eqation")
+        else:
+            print("absurde expression")
     else:
-        print("absurde expression")
-else:
-    print(eq_poly, "= 0")
-    print(f"eqations of degree {eq_poly.deg} are not suported")
+        print(eq_poly, "= 0")
+        print(f"eqations of degree {eq_poly.deg} are not suported")
+except Exception as e:
+    print(e)
 
 #TODO ./computor.py "-10 = -1(x^2 + 0)"
