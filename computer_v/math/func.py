282f01ef403c1601e58a09d139838f112b044612
ayoubyt <ayoub.aguert@gmail.com>
2021-03-18 12:05:23 +0100

    add

--- computer_v/math/func.py
+++ computer_v/math/func.py
@@ -1,15 +1,5 @@
-# the first letter of the function represents the algorithm or the method
-# b : Babylonian method
-# t : ternary operator
-
 def bsqrt(number):
-    """
-        calculating square root using Babylonian method
-        it's correct up to 14 after decimial
-    """
-    # first guess
     x = number
-    # (number/<first guess> = 1)
     y = 1
     precision = 1e-15
     while (abs(x - y) / abs(x) > precision):
